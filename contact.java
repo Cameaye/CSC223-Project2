88ae73b15e0ad310540c8b51a973b20ea071d1df
Cameron Ayers <cayers0704@gmail.com>
2025-09-29 11:26:15 -0400

    Init contact class

---
+++ contact.java
@@ -0,0 +1,51 @@
+public class contact{
+
+  private String firstName;
+  private String lastName;
+  private String phoneNumber;
+  private String address;
+
+  public contact(){
+    firstName = "";
+    lastName = "";
+    phoneNumber = "";
+    address = "";
+  }
+
+  public contact(String firstName, String lastName, String phoneNumber, String address){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+    this.address = address;
+  }
+
+  public String getFirstName(){
+    return firstName;
+  }
+  public String getLastName(){
+    return lastName;
+  }
+  public String getPhoneNumber(){
+    return phoneNumber;
+  }
+  public String getAddress(){
+    return address;
+  }
+
+
+  public void setFirstName(String firstName){
+    this.firstName = firstName;
+  }
+  public void setLastName(String lastName){
+    this.lastName = lastName;
+  }
+  public void setPhoneNumber(String phoneNumber){
+    this.phoneNumber = phoneNumber;
+  }
+  public void setAddress(String address){
+    this.address = address;
+  }
+
+
+}
+
